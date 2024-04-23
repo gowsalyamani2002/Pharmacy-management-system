f4ee679abc3bd43b2c13a46cc394953d00b76c08
gowsalyamani2002 <85476547+gowsalyamani2002@users.noreply.github.com>
2024-04-23 11:42:44 +0530

    Add files via upload

---
+++ Main/Models/PharmacyManagementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.pharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyManagementProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyManagementProjectApplication.class, args);
+	}
+
+}
