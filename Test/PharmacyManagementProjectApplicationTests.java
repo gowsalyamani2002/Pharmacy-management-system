d66e4f112c6b7ee3367c836faf064b3bcf599ba3
gowsalyamani2002 <85476547+gowsalyamani2002@users.noreply.github.com>
2024-04-23 11:57:21 +0530

    Add files via upload

---
+++ Test/PharmacyManagementProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyManagementProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
